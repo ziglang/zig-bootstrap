2f31acbc8719a39bfee985c9810043dd53a955f8
Timon Kruiper <timonkruiper@gmail.com>
2021-02-28 14:53:37 +0000

    update LLVM sources to llvmorg-12.0.0-rc2

---
+++ lld/test/ELF/linkerscript/nobits-offset.s
@@ -0,0 +1,28 @@
+# REQUIRES: x86
+# RUN: llvm-mc -filetype=obj -triple=x86_64-pc-linux %s -o %t.o
+# RUN: echo "SECTIONS { \
+# RUN:         .sec1 (NOLOAD) : { . += 1; } \
+# RUN:         .bss           : { *(.bss) } \
+# RUN:       };" > %t.script
+# RUN: ld.lld %t.o -T %t.script -o %t
+# RUN: llvm-readelf -S -l %t | FileCheck %s
+
+## If a SHT_NOBITS section is the only section of a PT_LOAD segment,
+## p_offset will be set to the sh_offset field of the section. Check we align
+## sh_offset to sh_addr modulo max-page-size, so that p_vaddr=p_offset (mod
+## p_align).
+
+# CHECK:      Name  Type     Address          Off     Size
+# CHECK-NEXT:       NULL     0000000000000000 000000  000000
+# CHECK-NEXT: .text PROGBITS 0000000000000000 000158  000000
+# CHECK-NEXT: .sec1 NOBITS   0000000000000000 000158  000001
+# CHECK-NEXT: .bss  NOBITS   0000000000000400 000400  000001
+
+# CHECK:      Type Offset   VirtAddr           PhysAddr           FileSiz  MemSiz   Flg Align
+# CHECK-NEXT: LOAD 0x000400 0x0000000000000400 0x0000000000000400 0x000000 0x000001 RW  0x1000
+
+# CHECK:      00 .bss {{$}}
+
+.bss
+.p2align 10
+.byte 0
