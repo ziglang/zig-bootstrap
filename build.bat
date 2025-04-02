dd704bda8788e4483cd3969df7d954c23b9a212e
Andrew Kelley <andrew@ziglang.org>
2025-04-02 13:04:36 -0700

    update zig sources to 0.14.0

--- build.bat
+++ build.bat
@@ -35,7 +35,7 @@ if "%VSCMD_ARG_HOST_ARCH%"=="x86" set OUTDIR=out-win-x86
 
 set ROOTDIR=%~dp0
 set "ROOTDIR_CMAKE=%ROOTDIR:\=/%"
-set ZIG_VERSION="0.14.0-dev.3299+31f353cd9"
+set ZIG_VERSION="0.14.0"
 set JOBS_ARG=
 
 pushd %ROOTDIR%
